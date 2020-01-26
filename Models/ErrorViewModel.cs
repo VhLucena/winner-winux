bc902f74f0b18f530d7d923c1fa1e543d52564ab
Vinicius Santos <vilucenasantos@gmail.com>
2020-01-26 12:17:05 -0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Winner.Winux.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
